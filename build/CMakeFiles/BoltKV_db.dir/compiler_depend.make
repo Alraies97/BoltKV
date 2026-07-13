4c5681783702571d44338612eede123b9751a234
alraies <alraies@alraies-dell.alraies-dell>
2026-07-13 19:18:48 +0300

    some cleaning

--- build/CMakeFiles/BoltKV_db.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for BoltKV_db.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alraies/Projects/BoltKV
-# Build directory: /home/alraies/Projects/BoltKV/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
